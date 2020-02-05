1c5b23b992a7074c2d3479a5413476453b5b974b
adavijit <adavijit@yahoo.com>
2020-02-06 03:22:23 +0530

    Updates

---
+++ precache-manifest.f64702fb1a4fc19481deada4fa62031a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6efc56a1d85b4edcc7d490d5871f7190",
+    "url": "/TechTrix20/index.html"
+  },
+  {
+    "revision": "9e94f239dd7a8a9454db",
+    "url": "/TechTrix20/static/css/main.ea7aa732.chunk.css"
+  },
+  {
+    "revision": "e6f94f94ebaa6d60c7e8",
+    "url": "/TechTrix20/static/js/2.9aa4c56d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/TechTrix20/static/js/2.9aa4c56d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e94f239dd7a8a9454db",
+    "url": "/TechTrix20/static/js/main.7d0ed912.chunk.js"
+  },
+  {
+    "revision": "1ac2f33d67bbf37369c8",
+    "url": "/TechTrix20/static/js/runtime-main.86c7f95f.js"
+  }
+]);
